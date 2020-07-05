936c1b6be260b77663f5c7aa8dcc125295ad0893
FinchyG <lagaunt120@yahoo.com>
2020-07-05 10:28:05 +0100

    Third example added

---
+++ AdderSubtracter/Operations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdderSubtracter
+{
+    class Operations
+    {
+        public static void Adder(int x, int y)
+        {
+            Console.WriteLine("The sum of the two numbers is {0}", x + y);
+        }
+
+        public static void Subtracter(int x, int y)
+        {
+            Console.WriteLine("The difference of the two numbers is {0}", x - y);
+        }
+    }
+}
---
+++ AdderSubtracter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AdderSubtracter
+{
+    class Program
+    {
+        public delegate void MathsOperations(int x, int y);
+        static void Main(string[] args)
+        {
+            MathsOperations MathsOps = Operations.Adder;
+            MathsOps += Operations.Subtracter;
+
+            Console.WriteLine("Please enter first number:");
+            int x = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Please enter first number:");
+            int y = Convert.ToInt16(Console.ReadLine());
+
+            MathsOps(x,y);
+
+            MathsOps -= Operations.Adder;
+            MathsOps -= Operations.Subtracter;
+        }
+    }
+}
