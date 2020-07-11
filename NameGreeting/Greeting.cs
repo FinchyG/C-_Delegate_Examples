58dd6e17b1b31be94b8293773ed564d4d4242cb3
FinchyG <lagaunt120@yahoo.com>
2020-07-11 21:30:07 +0100

    + new example

---
+++ NameGreeting/Greeting.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NameGreeting
+{
+    class Greeting
+    {
+        public static string GreetingMethod(string name)
+        {
+            return "Hello " + name;
+        }
+    }
+}
---
+++ NameGreeting/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NameGreeting
+{
+    public delegate string GreetingDelegate(string str);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GreetingDelegate g1 = new GreetingDelegate(Greeting.GreetingMethod);
+
+            Console.WriteLine(g1("Luke"));
+        }
+    }
+}
